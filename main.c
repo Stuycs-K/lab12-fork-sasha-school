04facc39c46fd1e1e1672ba4f0e9b0a96485c3c8
Sasha (Alex) Murokh <144375939+sasha-school@users.noreply.github.com>
2024-11-26 22:24:04 -0500

    ranodm isnt working what

--- main.c
+++ main.c
@@ -10,17 +10,26 @@
 int main(){
     pid_t p1;
     pid_t p2;
+    printf("%d about to create 2 child processes\n",getpid()); 
     p1 = fork();
     if (p1==0){
-        printf("child 1 pid: %d\n",getpid());
+        int random1;
+        random1 = rand()%5+1;
+        printf("%d %d sec\n",getpid(),random1);
+        sleep(random1);
+        printf("%d finished after %d sec\n",getpid(),random1);
     }
     else {
         p2 = fork();
         if (p2==0){
-            printf("child 2 pid: %d\n",getpid());    
+            int random2;
+            random2 = rand()%5+1;
+            printf("%d %d sec\n",getpid(),random2);
+            sleep(random2);
+            printf("%d finished after %d sec\n",getpid(),random2);
         }
         else{
-            printf("parent pid: %d\n",getpid()); 
+            
         }
     }
     
