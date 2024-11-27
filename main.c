e4f86129a8caddf1bee631befd46996288f4d75f
Sasha (Alex) Murokh <144375939+sasha-school@users.noreply.github.com>
2024-11-26 22:04:33 -0500

    figured out 2 children

--- main.c
+++ main.c
@@ -8,5 +8,17 @@
 #include <sys/wait.h>
 
 int main(){
-
+    pid_t p1;
+    pid_t p2;
+    p1 = fork();
+    p2 = fork();
+    if (p1==0){
+        printf("child 1");
+    }
+    else if (p2==0){
+        printf("child 2");
+    }
+    else{
+        printf("parent");
+    }
 }
