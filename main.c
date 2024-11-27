0e9f74826a642120aae1bcd8199ae013afa04920
Sasha (Alex) Murokh <144375939+sasha-school@users.noreply.github.com>
2024-11-26 22:34:24 -0500

    random works now ???

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(){
     printf("%d about to create 2 child processes\n",getpid()); 
     p1 = fork();
     if (p1==0){
+        srand( time(NULL) );
         int random1;
         random1 = rand()%5+1;
         printf("%d %d sec\n",getpid(),random1);
@@ -22,14 +23,19 @@ int main(){
     else {
         p2 = fork();
         if (p2==0){
-            int random2;
-            random2 = rand()%5+1;
-            printf("%d %d sec\n",getpid(),random2);
-            sleep(random2);
-            printf("%d finished after %d sec\n",getpid(),random2);
+            srand( time(NULL) +1);
+            int random1;
+            random1 = rand()%5+1;
+            printf("%d %d sec\n",getpid(),random1);
+            sleep(random1);
+            printf("%d finished after %d sec\n",getpid(),random1);
         }
         else{
-            
+            int status1;
+            pid_t child;
+            child = wait(&status1);
+            printf("Main Process %d is done. Child %d slept for x sec\n", getpid(), child);
+
         }
     }
     
