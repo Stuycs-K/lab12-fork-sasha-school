746c182022f4a31f0c331dd60c693bdeddb0b23a
Sasha (Alex) Murokh <144375939+sasha-school@users.noreply.github.com>
2024-11-26 22:39:51 -0500

    fixed

--- main.c
+++ main.c
@@ -13,28 +13,30 @@ int main(){
     printf("%d about to create 2 child processes\n",getpid()); 
     p1 = fork();
     if (p1==0){
-        srand( time(NULL) );
+        srand( time(NULL) ); //seed
         int random1;
         random1 = rand()%5+1;
         printf("%d %d sec\n",getpid(),random1);
         sleep(random1);
         printf("%d finished after %d sec\n",getpid(),random1);
+        return random1; // return secs
     }
     else {
         p2 = fork();
         if (p2==0){
-            srand( time(NULL) +1);
+            srand( time(NULL) +1); //re-seed
             int random1;
             random1 = rand()%5+1;
             printf("%d %d sec\n",getpid(),random1);
             sleep(random1);
             printf("%d finished after %d sec\n",getpid(),random1);
+            return random1;  //return secs
         }
         else{
             int status1;
             pid_t child;
             child = wait(&status1);
-            printf("Main Process %d is done. Child %d slept for x sec\n", getpid(), child);
+            printf("Main Process %d is done. Child %d slept for %d sec\n", getpid(), child, WEXITSTATUS( status1 )); //use return status
 
         }
     }
